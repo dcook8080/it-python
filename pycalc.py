10ac9047be356c23f0b3a988d8e75a27bbada43a
Dylan Cook <dylanjcook73@gmail.com>
2019-11-20 11:40:30 -0500

    update

--- pycalc.py
+++ pycalc.py
@@ -3,24 +3,31 @@ import math
 banner("Pyhtagorean Calculator" , "Dylan Cook")
 print("The Pythagorean Calculator will help you find the missing side lengths of a triangle.")
 print("Enter the lengths of all of the sides you know, if you do not know the length then leave it blank")
+while True:
+    a = input("a = ")
+    b = input("b = ")
+    c = input("c = ")
 
-a = input("a = ")
-b = input("b = ")
-c = input("c = ")
+    if a == "":
+        b = float(b)
+        c = float(c)
+        a = float(math.sqrt(c*c - b*b))
+        print(f"Your missing side is {a}")
 
-if a == "":
-    b = float(b)
-    c = float(c)
-    a = float(math.sqrt(c*c - b*b))
-    print(f"Your missing side is {a}")
+    elif b == "":
+         a = float(a)
+         c = float(c)
+         b = float(math.sqrt(c*c - a*a))
+         print(f"Your missing side is {b}")
 
-elif b == "":
-     a = float(a)
-     c = float(c)
-     b = float(math.sqrt(c*c - a*a))
-     print(f"Your missing side is {b}")
-elif c == "":
-    a = float(a)
-    b = float(b)
-    c = float(math.sqrt(a*a + b*b))
-    print(f"Your missing side is {c}")
+    elif c == "":
+        a = float(a)
+        b = float(b)
+        c = float(math.sqrt(a*a + b*b))
+        print(f"Your missing side is {c}")
+
+    if input("Would you like to solve another triangle? (Y/N) ") == 'Y':
+        continue
+    else:
+        break
+print("Thank you for using the Pythagorean Calculator!")
